3d944d8c67798d74094e3664a71ccb8c41d7787f
Nagehan1 <uyumaznagehan@gmail.com>
2023-01-31 23:16:45 +0000

    not solved

---
+++ js/Is it a Set.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// set! is a card game where you compete with other players, to find out who can find a set of cards first.
+// Your task is to write a function that checks if a collection of three input cards qualifies as a set.
+
+// The cards
+// Every card has one, two or three symbols in it. A symbol has three distinct features:
+
+// Shape (either diamond, snake or capsule)
+// Colour (either green, blue or red)
+// Pattern (either blank, striped or solid)
